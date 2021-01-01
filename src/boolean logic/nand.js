0a20b0a209553aa028e08edbf5560235950c8993
Tomo Yanagi <hideorseekyes+github@gmail.com>
2021-01-02 01:33:23 +0900

    implement nand function

---
+++ src/boolean logic/nand.js
@@ -0,0 +1,16 @@
+const {and, not} = require('./BooleanFunction');
+const nand = (a, b) => not(and(a,b));
+
+exports.nandOr = (a, b) => {
+  return nand(nand(a, a), nand(b, b));
+};
+exports.nandAnd = (a, b) => {
+  return nand(nand(b, b), nand(a, a));
+  // return nand(
+  //   nand(nand(nand(a, a), nand(a, a)), nand(b, b)),
+  //   nand(nand(nand(b, b), nand(b, b)), nand(a, a))
+  // );
+};
+exports.nandNot = (a) => {
+  return nand(nand(a, a), nand(a,a));
+};
